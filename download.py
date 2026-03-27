db590f8f8613f0fcfc56732201ed0f7918e5475a
Avnish Mishra <mavnish604@gmail.com>
2026-03-28 00:20:41 +0530

    changed something a bit :)

--- download.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("mohamadabouali1/mri-brain-tumor-dataset-4-class-7023-images")
-
-print("Path to dataset files:", path)
